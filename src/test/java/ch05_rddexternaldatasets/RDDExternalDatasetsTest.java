e9fe74481e22f97ab7c555c4767d34ad8aa976b7
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2025-06-27 18:01:46 +0000

    using method sources

--- src/test/java/ch05_rddexternaldatasets/RDDExternalDatasetsTest.java
+++ src/test/java/ch05_rddexternaldatasets/RDDExternalDatasetsTest.java
@@ -2,8 +2,14 @@ package ch05_rddexternaldatasets;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,6 +25,7 @@ public class RDDExternalDatasetsTest {
             "src\\test\\resources\\1000words.txt",
             "src\\test\\resources\\wordslist.txt.gz",
     })
+    @DisplayName("Testing loading local Textfiles into spark rdd using value source")
     public void testLoadingLocalTextFileIntoSparkRDD(final String localFilePath) {
         try(final var sparkContext = new JavaSparkContext(sparkConf)){
             final var myRdd = sparkContext.textFile(localFilePath);
@@ -30,6 +37,34 @@ public class RDDExternalDatasetsTest {
         }
 
     }
+
+    // test using method source
+    @ParameterizedTest
+    @MethodSource("getFilePath")
+    @DisplayName("Testing loading local Textfiles into spark rdd using method source")
+    public void testLoadingLocalTextFileIntoSparkRDDMethodSource(final String localFilePath) {
+        try(final var sparkContext = new JavaSparkContext(sparkConf)){
+            final var myRdd = sparkContext.textFile(localFilePath);
+
+            System.out.println("Total lines in the file: " + myRdd.count());
+            // printing the first 10
+            myRdd.take(10).forEach(System.out::println);
+            System.out.println("++++++++++++++++++++++++++");
+        }
+
+    }
+
+
+
+
+    private static Stream<Arguments> getFilePath(){
+
+        return Stream.of(
+                Arguments.of("src\\test\\resources\\1000words.txt"),
+                Arguments.of("src\\test\\resources\\wordslist.txt.gz")
+        );
+    }
+
 
 
 }
